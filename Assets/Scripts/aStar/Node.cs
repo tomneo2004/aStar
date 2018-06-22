3fca8e196f2926b580f7aca26bde43c619cafb72
NelsonPunch <tomneo2004@gmail.com>
2018-06-22 20:58:38 +1000

    - Basic Node funtionality "Add", "Remove" and "Find"

--- Assets/Scripts/aStar/Node.cs
+++ Assets/Scripts/aStar/Node.cs
@@ -42,6 +42,114 @@ namespace NP.aStarPathfinding{
 		public Node(Graph graph) : base(){
 
 			_graph = graph;
+			_connections = new List<Connection> ();
+		}
+
+		/**
+		 * Add connection from this node to another node
+		 * 
+		 * Specify connection type to undirect or direct. Default is Undirect
+		 **/
+		public Connection AddConnection(Node toNode, ConnectionType connType = ConnectionType.Undirect){
+
+			if (toNode == null) {
+				#if DEBUG
+				Debug.LogError("Unable to add connection. Given node is null");
+				#endif
+
+				return null;
+			}
+
+			//do not add connection if it is already exist
+			Connection conn = null;
+			conn = FindConnection (toNode);
+
+			if (conn != null) {
+				#if DEBUG
+				Debug.LogWarning("Connection is exist, we do not add connection");
+				#endif
+
+				return conn;
+			}
+
+			//create new connection to another node
+			conn = new Connection(this, toNode);
+
+			//add to connection list
+			_connections.Add(conn);
+
+			//if connection is undirect
+			if (connType == ConnectionType.Undirect) {
+
+				//tell another node to add connection to this node but only one way
+				toNode.AddConnection (this, ConnectionType.Direct);
+			}
+
+			return conn;
+		}
+
+		/**
+		 * Find connection from this node to another node
+		 **/
+		public Connection FindConnection(Node toNode){
+
+			Connection found = null;
+
+			IEnumerator ie = _connections.GetEnumerator ();
+			while (ie.MoveNext ()) {
+
+				//found
+				if (((Connection)ie.Current).To.Id == toNode.Id) {
+
+					found = (Connection)ie.Current;
+					break;
+				}
+			}
+
+			return found;
+		}
+
+		/**
+		 * Remove connection from this node to another node
+		 * 
+		 * Specify connection type to undirect or direct. Default is Direct(one way)
+		 **/
+		public void RemoveConnection(Node toNode, ConnectionType connType = ConnectionType.Direct){
+
+			int connIndex = -1;
+			int currIndex = 0;
+
+			IEnumerator ie = _connections.GetEnumerator ();
+			while (ie.MoveNext ()) {
+
+				//found
+				if (((Connection)ie.Current).To.Id == toNode.Id) {
+
+					connIndex = currIndex;
+					break;
+				}
+
+				currIndex++;
+			}
+
+			//has connection to another node, remove it otherwise do nothing
+			if (connIndex >= 0) {
+				
+				_connections.RemoveAt (connIndex);
+
+				if (connType == ConnectionType.Undirect) {
+
+					//tell another node to remove connection to this node
+					toNode.RemoveConnection (this, ConnectionType.Direct);
+				}
+
+				return;
+			}
+
+			#if DEBUG
+			Debug.LogWarning("There is no connection to another node, we have nothing to remove");
+			#endif
+				
 		}
 
 	}
